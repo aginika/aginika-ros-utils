c57436b9507b112e46c56030cffd501cd9c6dc99
aginika <inagaki@inagaki.mail>
2014-04-29 08:43:30 +0900

    update ros-bash-setup.sh

--- scripts/ros-bash-setup.sh
+++ scripts/ros-bash-setup.sh
@@ -3,3 +3,29 @@
 source `rospack find aginika-ros-utils`/scripts/complitions/robot_complete.sh
 source `rospack find aginika-ros-utils`/scripts/complitions/rosdep-complition.sh
 source `rospack find aginika-ros-utils`/scripts/complitions/wstool-complition.sh
+
+##alias or add commands
+cv (){
+   case "$1" in
+   "fuerte" ) source /opt/ros/fuerte/setup.bash;source ~/ros/fuerte/setup.bash;;
+   "groovy" ) source /opt/ros/groovy/setup.bash;source ~/ros/groovy/setup.bash;;
+   "hydro"  ) source /opt/ros/hydro/setup.bash;source ~/ros/hydro/devel/setup.bash;;
+   esac
+}
+
+alias rosc='cd ~/ros/hydro/src' ##ros src
+alias rosh='cd ~/ros/hydro'     ##ros homespace
+
+cmb (){
+    pushd .;
+    rosh;
+    catkin_make $1;
+    popd;
+}
+
+#roseus setting
+alias roseus='rlwrap roseus'
+
+#rosed setting
+export EDITOR='emacs -nw'
+
