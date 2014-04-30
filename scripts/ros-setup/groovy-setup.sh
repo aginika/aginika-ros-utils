543e0f24fbfefb30b85ded69e561200a278d4df6
aginika <inagaki@inagaki.mail>
2014-04-30 14:51:32 +0900

    update file

--- scripts/ros-setup/groovy-setup.sh
+++ scripts/ros-setup/groovy-setup.sh
@@ -1,1 +1,8 @@
-ln -s -b `rospack find aginika-ros-utils`/configs/groovy.rosinstall ~/ros/groovy//.rosinstall
+## add the rosinstall link
+ln -s -b `rospack find aginika-ros-utils`/configs/groovy.rosinstall ~/ros/groovy/.rosinstall
+
+## add a bash util line in .bashrc
+echo -e "##ros utils-bash\n" >> ~/.bashrc
+echo -e "source /opt/ros/groovy/setup.bash\n" >> ~/.bashrc
+echo -e "source ~/ros/ros/groovy/setup.bash\n" >> ~/.bashrc
+echo -e "source \`rospack find aginika-ros-utils\`/scripts/ros-bash-utils.sh\n" >> ~/.bashrc
