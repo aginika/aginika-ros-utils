05fc90cc72ff2b04dd42b33a3e57e381c644783f
aginika <inagaki@inagaki.mail>
2014-05-11 21:55:37 +0900

    add groovy_catkin.rosinstlal

--- scripts/ros-bash-setup.sh
+++ scripts/ros-bash-setup.sh
@@ -1,8 +1,8 @@
 
 ##Source the util ros sh files
-source `rospack find aginika-ros-utils`/scripts/complitions/robot_complete.sh
-source `rospack find aginika-ros-utils`/scripts/complitions/rosdep-complition.sh
-source `rospack find aginika-ros-utils`/scripts/complitions/wstool-complition.sh
+source `rospack find aginika_ros_utils`/scripts/complitions/robot_complete.sh
+source `rospack find aginika_ros_utils`/scripts/complitions/rosdep-complition.sh
+source `rospack find aginika_ros_utils`/scripts/complitions/wstool-complition.sh
 
 ##alias or add commands
 cv (){
--- scripts/ros-setup/groovy-setup.sh
+++ scripts/ros-setup/groovy-setup.sh
@@ -1,6 +1,9 @@
 ## add the rosinstall link
 ln -s -b `rospack find aginika-ros-utils`/configs/groovy.rosinstall ~/ros/groovy/.rosinstall
 
+if [ -e ~/ros/groovy_catkin/src ]; then
+    ln -s -b `rospack find aginika-ros-utils`/configs/groovy_catkin.rosinstall ~/ros/groovy_catkin/src/.rosinstall
+
 ## add a bash util line in .bashrc
 echo -e "##ros utils-bash\n" >> ~/.bashrc
 echo -e "source /opt/ros/groovy/setup.bash\n" >> ~/.bashrc
